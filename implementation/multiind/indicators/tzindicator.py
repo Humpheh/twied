9c6d160703e8de8e6354ab80e37578e323d08dd4
Humphrey Shotton <humpheh@gmail.com>
2016-02-13 12:47:45 +0000

    Added caching of tz and tzoff indicators
    Greatly reduces the run time of the algorithm + solves a bottleneck

--- implementation/multiind/indicators/tzindicator.py
+++ implementation/multiind/indicators/tzindicator.py
@@ -12,26 +12,32 @@ class TZIndicator(Indicator):
     def __init__(self, config):
         self.polydb_url = config.get("multiindicator", "gadm_polydb_path")
         self.weight = config.getfloat("mi_weights", "TZ")
+        self.history = {}
 
     def get_loc(self, tz):
         if tz is None:
             return []
+
+        if tz in self.history:
+            return self.history[tz]
 
         # setup db connection
         tzpoly = TZPolyInterface(self.polydb_url)
 
         # Check if falls in america
         if 'Pacific Time (US & Canada)' in tz:
-            return tzpoly.get_polys_america('PST', self.get_weight(1))
+            result = tzpoly.get_polys_america('PST', self.get_weight(1))
         elif 'Eastern Time (US & Canada)' in tz:
-            return tzpoly.get_polys_america('EST', self.get_weight(1))
+            result = tzpoly.get_polys_america('EST', self.get_weight(1))
         elif 'Central Time (US & Canada)' in tz:
-            return tzpoly.get_polys_america('CST', self.get_weight(1))
+            result = tzpoly.get_polys_america('CST', self.get_weight(1))
+        else:
+            # Format timezone a little better
+            tz = re.sub(r'\([^)]*\)', '', tz).strip().replace(' ', '_')
 
-        # Format timezone a little better
-        tz = re.sub(r'\([^)]*\)', '', tz).strip().replace(' ', '_')
+            result = tzpoly.get_polys(tz, self.get_weight(1))
 
-        result = tzpoly.get_polys(tz, self.get_weight(1))
+        self.history[tz] = result
 
         tzpoly.destroy()
         return result
--- implementation/multiind/indicators/tzoffindicator.py
+++ implementation/multiind/indicators/tzoffindicator.py
@@ -10,14 +10,20 @@ class TZOffsetIndicator(Indicator):
     def __init__(self, config):
         self.polydb_url = config.get("multiindicator", "gadm_polydb_path")
         self.weight = config.getfloat("mi_weights", "TZ")
+        self.history = {}
 
     def get_loc(self, offset):
         if offset is None:
             return []
 
+        if offset in self.history:
+            return self.history[offset]
+
         # setup db connection
         tzpoly = TZPolyInterface(self.polydb_url)
         result = tzpoly.get_polys_offset(offset, self.get_weight(1))
         tzpoly.destroy()
+
+        self.history[offset] = result
 
         return result
