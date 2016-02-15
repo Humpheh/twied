e9455a904e0e1bcdff02d0f3152ee5a66a153c1c
Humphrey Shotton <humpheh@gmail.com>
2016-02-15 11:26:11 +0000

    Readied exception repeat

--- collection/twicol/__init__.py
+++ collection/twicol/__init__.py
@@ -37,3 +37,24 @@ def setup_logger(filename):
     logging.info("-----------")
     logging.info("Initialised logging for: {0}".format(__main__.__file__))
     logging.info("-----------")
+
+def exception_repeat(func):
+    """
+    Function decorator that retrys the function if it throws an exception
+    after a short interval. Interval grows exponentially the more times
+    the function throws an exception. Initial time is 0.5 seconds.
+    """
+    def func_wrapper(timer = 0.5):
+        try:
+            func()
+        except:
+            try:
+                time.sleep(timer)
+            except:
+                return
+            err = sys.exc_info()[0]
+            ntimer = timer * 2 if timer <= 32 else 64
+            logging.warning ("Exception caught in repeat method: {0}".format(err))
+            logging.warning ("Waiting for {0}s before retrying...".format(ntimer))
+            func_wrapper(ntimer)
+    return func_wrapper
