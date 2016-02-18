9f127d04705d6fad159495c32d4057811515f58f
Humphrey Shotton <humpheh@gmail.com>
2016-02-18 00:04:13 +0000

    Initial event detection commit

---
+++ implementation/eventec/clusterupdater.py
@@ -0,0 +1,5 @@
+
+class ClusterUpdater:
+    def __init__(self):
+        pass
+
---
+++ implementation/eventec/tweetcluster.py
@@ -0,0 +1,5 @@
+
+class TweetCluster:
+    def __init__(self, tweets, centre):
+        self.tweets = tweets
+        self.center = centre
