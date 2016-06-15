d4d393a715d0156a9748468421806fdb90f26286
Humphrey Shotton <humpheh@gmail.com>
2016-06-15 13:36:55 +0100

    Moved Sphinx doc build to gh-pages

--- doc/make.bat
+++ doc/make.bat
@@ -5,7 +5,9 @@ REM Command file for Sphinx documentation
 if "%SPHINXBUILD%" == "" (
 	set SPHINXBUILD=sphinx-build
 )
-set BUILDDIR=build
+set BUILDDIR=../../code-docs
+set PDFBUILDDIR=/tmp
+set PDF=../manual.pdf
 set ALLSPHINXOPTS=-d %BUILDDIR%/doctrees %SPHINXOPTS% source
 set I18NSPHINXOPTS=%SPHINXOPTS% source
 if NOT "%PAPER%" == "" (
